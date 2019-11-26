a0c7b1f9db0812d1a6a1224686912c90ff9c4867
Vika Tsukanova <victoriatsukanova@gmail.com>
2019-11-25 21:28:41 -0500

    Update habits array with a new habit and style the front end

--- src/App.js
+++ src/App.js
@@ -1,12 +1,34 @@
 import React, { useState, useEffect } from 'react';
 
+import { makeStyles } from '@material-ui/core/styles';
 import Button from '@material-ui/core/Button';
 import List from '@material-ui/core/List';
 import ListItem from '@material-ui/core/ListItem';
 import ListItemText from '@material-ui/core/ListItemText';
 import TextField from '@material-ui/core/TextField';
+import Container from '@material-ui/core/Container';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import Card from '@material-ui/core/Card';
+
+const useStyles = makeStyles({
+  root: {
+    display: 'flex',
+    alignItems: 'baseline',
+  },
+  newHabitField: {
+    flex: 4,
+    marginRight: 5
+  },
+  newHabitSubmitButton: {
+    flex: 1
+  }
+});
 
 function App(props) {
+  const classes = useStyles();
+
   const [habits, setData] = useState([]);
   const [newHabit, setNewHabit] = useState('')
 
@@ -38,41 +60,52 @@ function App(props) {
     })
 
     const newHabitResult = await response.json()
-    const newHabitList = [...habits, newHabitResult.data]
-    setData(newHabitList)
+    setData(habits.concat(newHabitResult.data))
     setNewHabit('')
-    console.log({newHabitResult})
   }
 
     return (
-      <div>
-        SmartHabit
-        <Button
-          variant="outlined"
-          onClick={handleButtonSubmit}
-        >
-          Add a Habit
-        </Button>
-        <TextField
-            required
-            id="filled-required"
-            label="Required"
-            onChange={handleHabitChange}
-            margin="normal"
-            variant="filled"
-            value={newHabit}
-          />
-        <List component="nav" aria-label="secondary mailbox folders">
-          {habits.map(habit => {
-            const { _id: id, name } = habit;
-            return (
-              <ListItem key={id}>
-                <ListItemText primary={name} />
-              </ListItem>
-            )
-          })}
-        </List>
-      </div>
+      <React.Fragment>
+        <AppBar position="static">
+          <Toolbar>
+            <Typography variant="h5">SmartHabit</Typography>
+          </Toolbar>
+        </AppBar>
+        <Container>
+          <div className={classes.root}>
+            <TextField
+              className={classes.newHabitField}
+              fullWidth
+              required
+              id="filled-required"
+              label="New Habit"
+              onChange={handleHabitChange}
+              margin="normal"
+              value={newHabit}
+            />
+            <Button
+              className={classes.newHabitSubmitButton}
+              fullWidth
+              variant="outlined"
+              onClick={handleButtonSubmit}
+            >
+              Add a Habit
+            </Button>
+          </div>
+          <Card>
+            <List component="nav" aria-label="secondary mailbox folders">
+              {habits.map(habit => {
+                const { _id: id, name } = habit;
+                return (
+                  <ListItem divider button key={id}>
+                    <ListItemText primary={name} />
+                  </ListItem>
+                )
+              })}
+            </List>
+          </Card>
+        </Container>
+      </React.Fragment>
     );
 }
 
